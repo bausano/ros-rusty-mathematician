ede25b28a62b5a28d9195bb980d23a43ee211430
Michael Bausano <michael@joinpouch.com>
2019-02-07 18:01:15 +0000

    Finished refactoring

--- src/main.py
+++ src/main.py
@@ -8,32 +8,29 @@ from core.subscriber import Subscriber
 
 # ROS Boot.
 if __name__ == '__main__':
-  try:
-    # Starts a new node with a random hash appended to its name.
-    rospy.init_node('trig_functions', anonymous=True)
+  # Starts a new node with a random hash appended to its name.
+  rospy.init_node('trig_functions', anonymous=True)
 
-    # Constructs a new publisher instance that emits messages to the turtle
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=50)
+  # Constructs a new publisher instance that emits messages to the turtle
+  pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=50)
 
-    # Draws x axis and then moves the turtle bot closer to the right edge.
-    prepare(pub)
+  # Draws x axis and then moves the turtle bot closer to the right edge.
+  prepare(pub)
 
-    # Constructs a new rate object on current thread that is used for publishing
-    # frequency. There is maximally one message published per tick.
-    thread = rospy.Rate(1)
+  # Constructs a new rate object on current thread that is used for publishing
+  # frequency. There is maximally one message published per tick.
+  thread = rospy.Rate(1)
 
-    # Subscriber instance.
-    subscriber = Subscriber(pub)
+  # Subscriber instance.
+  subscriber = Subscriber(pub)
 
-    # Creates loop that does not exit unless the core was shut down.
-    while not rospy.is_shutdown():
-      subscriber.tick(
-        math.pi / 12,
-        math.sin
-        # lambda x: x*x
-      )
+  # Creates loop that does not exit unless the core was shut down.
+  while not rospy.is_shutdown():
+    subscriber.tick(
+      math.pi / 12,
+      math.sin
+      # lambda x: x*x
+    )
 
-      thread.sleep()
-
-  except rospy.ROSInterruptException:
-    print('Connection to ROS interrupted.')
+    # Mimics frequency functionality.
+    thread.sleep()
