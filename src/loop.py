ceb456690ca6e56ef3503e16129c5db029f0c64e
Michael Bausano <michael@joinpouch.com>
2019-02-07 15:56:54 +0000

    fixed innit

--- src/loop.py
+++ src/loop.py
@@ -2,7 +2,7 @@ import math
 import rospy
 from geometry_msgs.msg import Twist
 
-dx = math.pi / 8
+dx = math.pi / 4
 f = math.sin
 
 _dxsq = dx * dx
@@ -14,7 +14,7 @@ def loop(pub, state):
 
   change = math.sqrt(_dxsq + math.pow(dy, 2))
 
-  delta = math.acos(dx / change) * math.pow(dy, 0)
+  delta = math.asin(dy / change)
 
   message = Twist()
   message.linear.x = change
@@ -29,39 +29,21 @@ def loop(pub, state):
 
   return state
 
-def move(message):
-  vel_msg = Twist()
-
-  #Receiveing the user's input
-  print("Let's move your robot")
-  speed = 2
-  distance = 3
-  isForward = True
-
-  #Checking if the movement is forward or backwards
-  if(isForward):
-    vel_msg.linear.x = abs(speed)
-  else:
-    vel_msg.linear.x = -abs(speed)
-  #Since we are moving just in x-axis
-  vel_msg.linear.y = 0
-  vel_msg.linear.z = 0
-  vel_msg.angular.x = 0
-  vel_msg.angular.y = 0
-  vel_msg.angular.z = 0
+def move(pub, distance, speed):
+  message = Twist()
+  message.linear.x = speed
 
   while not rospy.is_shutdown():
-    #Setting the current time for distance calculus
+    traveled = 0
     t0 = rospy.Time.now().to_sec()
-    current_distance = 0
 
-    #Loop to move the turtle in an specified distance
-    while(current_distance < distance):
-        #Publish the velocity
-        #Takes actual time to velocity calculus
-        t1=rospy.Time.now().to_sec()
-        #Calculates distancePoseStamped
-        current_distance= speed*(t1-t0)
-    #After the loop, stops the robot
-    vel_msg.linear.x = 0
-    #Force the robot to stop
+    while (math.fabs(traveled) < distance):
+      pub.publish(message)
+
+      t1 = rospy.Time.now().to_sec()
+      traveled = speed * (t1 - t0)
+
+    message.linear.x = 0
+    pub.publish(message)
+
+    break
